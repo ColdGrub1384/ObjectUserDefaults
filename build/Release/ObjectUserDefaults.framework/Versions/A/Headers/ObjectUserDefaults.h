51af5d440da3e9374d1ad0e6223731b19d428a47
Adrian Labbé <adrilabbelol@gmail.com>
2018-09-05 20:50:04 -0300

    Support for Objective-C

--- build/Release/ObjectUserDefaults.framework/Versions/A/Headers/ObjectUserDefaults.h
+++ build/Release/ObjectUserDefaults.framework/Versions/A/Headers/ObjectUserDefaults.h
@@ -6,5 +6,6 @@
 //  Copyright © 2018 Adrian Labbé. All rights reserved.
 //
 
-#import "ObjectUserDefaults-Swift.h"
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectUserDefaults/PublicHeader.h>
+
 
