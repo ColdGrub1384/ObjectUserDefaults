23da49093476b74e129758ad62680e58c3b2da5c
Adrian Labbé <adrilabbelol@gmail.com>
2018-09-05 20:03:55 -0300

    Remove build directory

--- build/ObjectUserDefaults.build/Release-iphoneos/ObjectUserDefaults.build/DerivedSources/ObjectUserDefaults_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char ObjectUserDefaultsVersionString[];
- extern const double ObjectUserDefaultsVersionNumber;
-
- const unsigned char ObjectUserDefaultsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ObjectUserDefaults  PROJECT:ObjectUserDefaults-1" "\n";
- const double ObjectUserDefaultsVersionNumber __attribute__ ((used)) = (double)1.;
--- build/ObjectUserDefaults.build/Release-iphoneos/ObjectUserDefaults.build/Objects-normal/arm64/ObjectUserDefaults_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/adrianlabbe/Desktop/ObjectUserDefaults/build/ObjectUserDefaults.build/Release-iphoneos/ObjectUserDefaults.build/DerivedSources/ObjectUserDefaults_vers.c
